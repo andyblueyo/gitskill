f4f6879f743817dd998a49777579dc95993cf605
Vincent M. van der Meulen <meulen@uw.edu>
2018-03-05 11:12:26 -0800

    Work with less rows and clarify vars

--- ds/data_prep.r
+++ ds/data_prep.r
@@ -12,6 +12,34 @@ repo_data_flat <- flatten(repo_data, recursive = TRUE) %>% sample_n(20, replace 
 # View(repo_data)
 
 print(repo_data[2,6])
-df <- data.frame(repo_data_flat[, c("name", "languages", "ownerType", "ownerName")]) 
+df_repo <- data.frame(repo_data_flat[, c("name", "languages", "ownerType", "ownerName")]) 
 
-View(df)
+z <- 1
+for(language in df_repo$languages){
+    if(length(language)>0){
+        
+        lines <- language[,"lines"]
+        lang <- language[,"name"]
+        if(length(lang)>0){
+            
+            for(i in 1:length(lang)){
+                print(z)
+                
+                df_repo[z, lang[i]] <- lines[i]
+            }
+        }
+    }
+    
+    z <- z + 1
+}
+
+drops <- c("languages")
+df_repo2 <- df_repo[, !(names(df_repo) %in% drops)] #%>% apply(2,as.character)
+df_repo2[is.na(df_repo2)] <- 0
+
+df_repo3 <- df_repo2 %>% 
+    group_by(ownerName) %>% 
+    filter() %>% 
+    summarize_all(funs(sum(as.numeric(.))))
+
+View(df_repo)
