64c27d93492fc1cfdb1841976088f1b09efb5e0a
Vincent M. van der Meulen <meulen@uw.edu>
2018-03-05 11:07:10 -0800

    Continue data prep

--- ds/data_prep.r
+++ ds/data_prep.r
@@ -5,44 +5,13 @@ library(tidyr)
 repo_data <- fromJSON("./data/repos.json")
 user_data <- fromJSON("./data/users.json")
 
+user_data_flat <- flatten(user_data, recursive = TRUE) %>% sample_n(20, replace = FALSE)
+repo_data_flat <- flatten(repo_data, recursive = TRUE) %>% sample_n(20, replace = FALSE)
 
-user_data <- flatten(user_data, recursive = TRUE)
-View(user_data)
-repo_data <- flatten(repo_data, recursive = TRUE)
-View(repo_data)
+# View(user_data)
+# View(repo_data)
 
 print(repo_data[2,6])
-df <- data.frame(repo_data[,c("name", "languages", "ownerType", "ownerName")])
+df <- data.frame(repo_data_flat[, c("name", "languages", "ownerType", "ownerName")]) 
 
-
-
-z <- 1
-for(language in df$languages){
-  if(length(language)>0){
-    
-    lines <- language[,"lines"]
-    lang <- language[,"name"]
-    if(length(lang)>0){
-      
-      for(i in 1:length(lang)){
-        print(z)
-        
-        df[z, lang[i]] <- lines[i]
-      }
-    }
-  }
-  
-  z <- z + 1
-}
-drops <- c("languages")
-df2 <- df[, !(names(df) %in% drops)] #%>% apply(2,as.character)
-df2[is.na(df2)] <- 0
-
-
-df3 <- df2 %>% 
-  group_by(ownerName) %>% 
-  filter() %>% 
-  summarize_all(funs(sum(as.numeric(.))))
-
-write.csv(x = df2, file = "./data/expanded.csv")
-write.csv(x = repo_data, file = "./data/repos.csv")
+View(df)
