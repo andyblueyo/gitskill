23165f7f1c9dddd68439894509f37b29e933df09
andyblueyo <abchen@uw.edu>
2018-03-05 22:27:11 -0800

    tidy df

--- ds/data_prep.r
+++ ds/data_prep.r
@@ -2,59 +2,17 @@ library(dplyr)
 library(jsonlite)
 library(tidyr)
 
-repo_data <- fromJSON("./data/repos.json")
-user_data <- fromJSON("./data/users.json")
+repo_data <- fromJSON("data/data/repos.json")
+user_data <- fromJSON("data/data/users.json")
 
-user_data_flat <- flatten(user_data, recursive = TRUE) #%>% sample_n(20, replace = FALSE)
-repo_data_flat <- flatten(repo_data, recursive = TRUE) #%>% sample_n(20, replace = FALSE)
+user_data_flat <- flatten(head(user_data, 100), recursive = TRUE) #%>% sample_n(20, replace = FALSE)
+repo_data_flat <- flatten(head(repo_data, 100), recursive = TRUE) #%>% sample_n(20, replace = FALSE)
 
 # View(user_data)
 # View(repo_data)
 
-df_repo <- data.frame(repo_data_flat[, c( "ownerName", "languages", "ownerType")]) 
+df_repo <- data.frame(repo_data_flat[, c( "ownerName", "name", "languages", "ownerType")]) 
+df_repo <- df_repo %>% unnest(languages) 
+df_repo <- df_repo %>% group_by(ownerName, name1) %>% summarise_at(c("lines"), sum)
 
 
-index <- 1
-#Loop through language lists
-for(language in df_repo$languages){
-  
-    if(length(language)>0){
-      
-        #If there are any languages in the list, get the name and the lines
-        lines <- language[,"lines"]
-        lang <- language[,"name"]
-        if(length(lang)>0){
-            
-            for(i in 1:length(lang)){
-                print(index)
-                #Add cell in column of the language and index of the person we want
-                df_repo[index, lang[i]] <- lines[i]
-            }
-        }
-    }
-    
-  index <- index + 1
-}
-
-# remove languages and set NAs to 0
-drops <- c("languages")
-df_repo2 <- df_repo[, !(names(df_repo) %in% drops)] #%>% apply(2,as.character)
-df_repo2[is.na(df_repo2)] <- 0
-
-# Group by user 
-df_repo2 <- df_repo2 %>% 
-    group_by(ownerName, ownerType) %>% 
-    summarize_all(funs(sum(as.numeric(.))))
-
-#make column names match for merge
-colnames(user_data_flat)[1] <- "ownerName"
-
-#Select JUST Users (not orgs) from both dataframes
-df_repo2 <- df_repo2 %>% filter(ownerType == "User")
-user_data_df <- data.frame(user_data_flat[, c( "ownerName", "userType","publicRepos", "ownedPrivateRepos", "totalPrivateRepos")]) %>% filter(user_data_flat$userType == "User")
-  
-#Merge 
-merged_dfs <- merge(df_repo3, user_data_flat, by = "ownerName", all = TRUE) 
-
-colnames(df4)
-View(df_repo)
