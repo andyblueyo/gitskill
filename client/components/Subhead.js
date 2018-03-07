f8c6bf5631aff851c3d6ea032464a9247e2283af
Vincent M. van der Meulen <meulen@uw.edu>
2018-03-07 12:00:17 -0800

    Set up basic structure

--- client/components/Subhead.js
+++ client/components/Subhead.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 const Subhead = ({ authors }) => {
-    let formattedAuthors = [applyStyles(true, authors[0])];
+    let formattedAuthors = [];
 
-    for (let i = 1; i < authors.length; i++) {
-        formattedAuthors.push(applyStyles(false, authors[i]));
+    for (let i = 0; i < authors.length; i++) {
+        switch (i) {
+            case 0:
+                formattedAuthors.push(applyStyles(authors[i], true, false));
+                break;
+            case authors.length - 1:
+                formattedAuthors.push(applyStyles(authors[i], false, true));
+                break;
+            default:
+                formattedAuthors.push(applyStyles(authors[i], false, false));
+        }
     }
 
     return (
@@ -14,12 +23,20 @@ const Subhead = ({ authors }) => {
     );
 };
 
-const applyStyles = (isFirst, author) => {
+const applyStyles = (author, isFirst, isLast) => {
+    let predecessor = "";
+
+    if (isLast) {
+        predecessor = " and ";
+    } else if (!isFirst) {
+        predecessor = ", ";
+    }
+
     return (
         <span>
-            {isFirst ? "" : ", "}
+            {predecessor}
             <a
-                className="black"
+                className="black bold sans-serif"
                 href={author.url}
                 target="_blank"
                 key={author.name}
