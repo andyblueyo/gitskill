d9d6923bb938dc0d26c1398c972bdcc8f8c74045
Vincent M. van der Meulen <meulen@uw.edu>
2018-03-07 01:08:24 -0800

    Experiment with basic responsive styles

--- client/components/default/action.js
+++ client/components/default/action.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 class Action extends React.PureComponent {
-  render() {
-    return (
-      <span {...this.props} className={'action'}>{this.props.children}</span>
-    );
-  }
+    render() {
+        return (
+            <span {...this.props} className={"action"}>
+                {this.props.children}
+            </span>
+        );
+    }
 }
 
 export default Action;
