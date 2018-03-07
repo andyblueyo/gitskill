01d886d2fe89dadef2888fef5843537c9f25ca80
Paul Vaden <pvaden@uw.edu>
2018-03-06 17:48:18 -0800

    added mergedcsv

--- ds/data_prep.r
+++ ds/data_prep.r
@@ -11,7 +11,7 @@ repo_data_flat <- flatten(repo_data, recursive = TRUE) #%>% sample_n(20, replace
 # View(user_data)
 # View(repo_data)
 
-df_repo <- data.frame(repo_data_flat[, c( "ownerName", "languages", "ownerType")]) 
+df_repo <- data.frame(repo_data_flat[, c( "ownerName", "languages", "ownerType", "stars", "forks")]) 
 
 
 index <- 1
@@ -26,7 +26,6 @@ for(language in df_repo$languages){
         if(length(lang)>0){
             
             for(i in 1:length(lang)){
-                print(index)
                 #Add cell in column of the language and index of the person we want
                 df_repo[index, lang[i]] <- lines[i]
             }
@@ -50,9 +49,8 @@ df_repo2 <- df_repo2 %>%
 colnames(user_data_flat)[1] <- "ownerName"
 
 #Select JUST Users (not orgs) from both dataframes
-df_repo2 <- df_repo2 %>% filter(ownerType == "User")
-user_data_df <- data.frame(user_data_flat[, c( "ownerName", "userType","publicRepos", "orgs")]) %>% 
-  filter(userType == "User") 
+#df_repo2 <- df_repo2 %>% filter(ownerType == "User")
+user_data_df <- data.frame(user_data_flat[, c( "ownerName", "userType","publicRepos", "orgs")]) 
   
 #Merge 
 merged_dfs <- merge(user_data_df, df_repo2, by = "ownerName", all = TRUE) %>% apply(2,as.character)
