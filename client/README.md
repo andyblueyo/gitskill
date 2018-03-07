9f80e04df3f9664dece91eb0b9fa6a7d8b518376
Vincent M. van der Meulen <meulen@uw.edu>
2018-03-07 00:26:16 -0800

    Add basic styles and components

---
+++ client/README.md
@@ -0,0 +1,8 @@
+#  Installation
+
+`yarn install`
+
+# Running the Client
+
+1. `gulp`
+2. `yarn start`
---
+++ client/components/Text.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Text = ({ children }) => {
+    return <p>{children}</p>;
+};
+
+module.exports = Text;
