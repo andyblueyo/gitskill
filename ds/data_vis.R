eafeb9ee767a7be8c36d9f9159d8483eb3360d62
andyblueyo <abchen@uw.edu>
2018-03-08 14:39:10 -0800

    add scatter plot

--- ds/data_vis.R
+++ ds/data_vis.R
@@ -11,15 +11,20 @@ colnames(repo_data)
 gather_data <- repo_data %>% select(-c(X)) %>% gather(language, line, Java:Zimpl)
 
 pls <- na.omit(gather_data)
-count_lang <- pls %>% filter(line != 0) %>% count(language)
- 
-total_lines <- pls %>% select(language, stars, forks, line, publicRepos) %>% filter(stars != 0) %>% group_by(language) %>% summarise_at(c("stars"), sum)
+count_lang <- gather_data %>% filter(line != 0) %>% count(language) %>% filter(n > 4400)
+
+count_lang <- gather_data %>% select(language, stars, forks, line, publicRepos) %>% filter(line != 0) %>% group_by(language) %>% count(stars)
+
+total_lines <- pls %>% select(language, stars, forks, line, publicRepos) %>% filter(stars != 0) %>% group_by(language) %>% summarise_at(c("lines"), sum)
 
 # bar plot of freq of counts of language
 bar_lang_count <- ggplot(data=count_lang, aes(x = language, y = n)) + geom_bar(stat = "identity") + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 5))
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 12), plot.title = element_text(hjust = 0.5)) + labs(x = "Languages", y = "Counts", title = "Frequency of Languages")
+bar_lang_count
 
 #scatter of stars and forks
-forks_star <- ggplot(data = gather_data, aes(x=forks, y = stars)) + geom_point()
+forks_star <- ggplot(data = pls, aes(x=forks, y = stars)) + geom_point() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 12), plot.title = element_text(hjust = 0.5)) + labs(x = "Number of forks", y = "Number of Stars", title = "Relationship with Stars and Forks")
+forks_star
 
 
