9ecafd9232282feb6917fe9ee99930f5dcead45a
andyblueyo <abchen@uw.edu>
2018-03-08 12:55:56 -0800

    sample plots

--- ds/data_vis.R
+++ ds/data_vis.R
@@ -10,9 +10,16 @@ colnames(repo_data)
 # Gather data with language and line count
 gather_data <- repo_data %>% select(-c(X)) %>% gather(language, line, Java:Zimpl)
 
-
-count_lang <- gather_data %>% filter(line != 0) %>%  count(language)
+pls <- na.omit(gather_data)
+count_lang <- pls %>% filter(line != 0) %>% count(language)
+ 
+total_lines <- pls %>% select(language, stars, forks, line, publicRepos) %>% filter(stars != 0) %>% group_by(language) %>% summarise_at(c("stars"), sum)
 
 # bar plot of freq of counts of language
-ggplot(data=count_lang, aes(x = language, y = n)) + geom_bar(stat = "identity") + 
+bar_lang_count <- ggplot(data=count_lang, aes(x = language, y = n)) + geom_bar(stat = "identity") + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 5))
+
+#scatter of stars and forks
+forks_star <- ggplot(data = gather_data, aes(x=forks, y = stars)) + geom_point()
+
+
